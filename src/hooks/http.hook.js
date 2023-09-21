fabe9b5825beac283778280d762bbbd59ca56846
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-09-21 19:05:59 +0300

    http.hook

---
+++ src/hooks/http.hook.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export const useHttp = () => {
+    const [error, setError] = useState(null);
+
+    const request = async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
+
+        try {
+            console.log(url);
+            console.log(body);
+            const response = await fetch(url, {method, body, headers});
+
+            console.log(response);
+
+            if(!response.ok) {
+                throw new Error(`Could not fetch ${url}, status: ${response.status}`);
+            }
+            
+            const data = await response.json();
+
+            console.log(data);
+
+            return data;
+
+        } catch (e) {
+            setError(e.message);
+            console.log(e);
+            throw e;
+        }
+    }
+
+    const clearError = () => setError(null);
+
+    return {request, error, clearError};
+}
